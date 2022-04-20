afaa5b3ce31c27480917226b9c42c2372e1d2e58
asher7u7 <jhonessvaquiro@colegioguanenta.edu.cogit config --listclearjhonessvaquiro@colegioguanenta.edu.cojhonessvaquiro@colegioguanenta.edu.co>
2022-04-20 07:57:51 -0500

    Desarrollo: programa terminado en python. asher7u7

---
+++ potencia.py
@@ -0,0 +1,21 @@
+"""ejercicio No.2:
+Programa para calcular x^y"""
+
+print("-------------------------------")
+print("------potencia de x a la y ----")
+print("-------------------------------")
+
+#inputs
+
+x= int(input("digite X: "))
+y= int(input("digite Y: "))
+
+
+#procedure
+
+z= x**y
+
+#output
+
+print("\nResultados:\n")
+print(str(x)+ " elevado a "+ str(y)+ " es igual a " +str(z))
